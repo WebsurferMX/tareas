23100ceb7fdfd951b3075a2673f1fa2444702878
fabricioAtSoultech <fasuarezd@gmail.com>
2023-03-08 21:20:23 -0600

    Sesion 2

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -1,25 +1,35 @@
-const getTareas = (req, res) => {
-    res.status(200).json({ mensaje: 'Mostrar las Tareas' })
-}
+const asyncHandler = require('express-async-handler')
+const Tarea = require('../models/tareaModel')
 
-const setTareas = (req, res) => {
+const getTareas = asyncHandler(async (req, res) => {
 
-    if (!req.body.nombre) {
+    const tareas = await Tarea.find()
+
+    res.status(200).json(tareas)
+})
+
+const setTareas = asyncHandler(async (req, res) => {
+
+    if (!req.body.texto) {
         //res.status(400).json({ mensaje: 'Favor de teclear la descripción de la tarea' })
         res.status(400)
         throw new Error('Favor de teclear una descripción para la tarea')
     }
 
-    res.status(201).json({ mensaje: 'Crear una tarea' })
-}
+    const tarea = await Tarea.create({
+        texto: req.body.texto
+    })
 
-const updateTareas = (req, res) => {
+    res.status(201).json(tarea)
+})
+
+const updateTareas = asyncHandler(async (req, res) => {
     res.status(200).json({ mensaje: `Modificar la tarea ${req.params.id}` })
-}
+})
 
-const deleteTareas = (req, res) => {
+const deleteTareas = asyncHandler(async (req, res) => {
     res.status(200).json({ mensaje: `Eliminar la tarea ${req.params.id}` })
-}
+})
 
 module.exports = {
     getTareas,
